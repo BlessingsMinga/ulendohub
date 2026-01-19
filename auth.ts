044fdf9b6154c51a3c430aaf6ea3f7f6bbaefb53
BlessingsMinga <blessingsminga11@gmail.com>
2026-01-19 10:32:40 +0200

    Next-Auth google sign in

---
+++ auth.ts
@@ -0,0 +1,11 @@
+import NextAuth from 'next-auth';
+import GoogleProvider from "next-auth/providers/google";
+import {PrismaAdapter} from "@auth/prisma-adapter";
+import { prisma } from "./lib/prisma";
+
+export const {auth, handlers, signIn, signOut} = NextAuth ({
+
+    providers: [GoogleProvider],
+    adapter: PrismaAdapter(prisma),
+
+});
